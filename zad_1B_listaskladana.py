8feab89e04e442d46d418cc18b4492e8562693cf
Depzster <147411937+Depzster@users.noreply.github.com>
2023-10-30 17:03:55 +0200

    Update zad_1B_listaskladana.py

--- zad_1B_listaskladana.py
+++ zad_1B_listaskladana.py
@@ -1,6 +1,6 @@
 def pomnoz_przez_dwa_listaskladana(lista):
     return [liczba * 2 for liczba in lista]
 lista=[5, 10, 15, 20, 25]
-wynik_skladana = pomnoz_przez_dwa_listaskladana(lista)
-print(wynik_skladana)
+wynik=pomnoz_przez_dwa_listaskladana(lista)
+print(wynik)
 
