ad8067561c5337087fd33e955af577f7a0825245
Depzster <147411937+Depzster@users.noreply.github.com>
2023-12-11 15:09:47 +0100

    Update zad_4.py

--- zad_4.py
+++ zad_4.py
@@ -1,10 +1,13 @@
 def check_sum(num1, num2, num3):
-    sum_of_first_two=num1+num2
-    return sum_of_first_two>=num3
-num1=int(input("Podaj 1 liczbę: "))
-num2=int(input("Podaj 2 liczbę: "))
-num3=int(input("Podaj 3 liczbę: "))
-result=check_sum(num1, num2, num3)
+    sum_of_first_two = num1 + num2
+    return sum_of_first_two >= num3
+
+
+num1 = int(input("Podaj pierwszą liczbę: "))
+num2 = int(input("Podaj drugą liczbę: "))
+num3 = int(input("Podaj trzecią liczbę: "))
+result = check_sum(num1, num2, num3)
+
 if result:
     print("Suma dwóch pierwszych liczb jest większa lub równa trzeciej.")
 else:
