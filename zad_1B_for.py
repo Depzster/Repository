e4aa37355ab02796ced0a2ce3c4ecc05a4a8a317
Depzster <147411937+Depzster@users.noreply.github.com>
2023-12-11 15:23:19 +0100

    Update and rename zad_1B_for.py to zad_2B_for.py

--- zad_1B_for.py
+++
@@ -1,8 +0,0 @@
-def pomnoz_przez_dwa_for(lista):
-    wynik=[]
-    for liczba in lista:
-        wynik.append(liczba*2)
-    return wynik
-lista=[1, 2, 3, 4, 5]
-wynik_for=pomnoz_przez_dwa_for(lista)
-print(wynik_for)
