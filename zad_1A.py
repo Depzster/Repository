9a60259c51f30adee3432cb5f2477eb0ca306e99
Depzster <147411937+Depzster@users.noreply.github.com>
2023-12-11 15:21:17 +0100

    Update zad_1A.py

--- zad_1A.py
+++ zad_1A.py
@@ -1,5 +1,7 @@
-def wyswietl_imiona(imiona):
-    for imie in imiona:
-        print(imie)
-lista = ["Patryk", "Wiktor", "Damian", "Jakub", "Bartek"]
-wyswietl_imiona(lista)
+def display_names(names):
+    for name in names:
+        print(name)
+
+
+name_list = ["Patryk", "Wiktor", "Damian", "Jakub", "Bartek"]
+display_names(name_list)
