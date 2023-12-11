968535263c6eec6ed20b2048dcce65aa0c5af50e
Depzster <147411937+Depzster@users.noreply.github.com>
2023-12-11 15:17:58 +0100

    Update zad_2.py

--- zad_2.py
+++ zad_2.py
@@ -1,49 +1,55 @@
+from datetime import datetime
+
+
 class Library:
     def __init__(self, city, street, zip_code, open_hours, phone):
-        self.city = city
-        self.street = street
-        self.zip_code = zip_code
-        self.open_hours = open_hours
-        self.phone = phone
+        self.city, self.street, self.zip_code = city, street, zip_code
+        self.open_hours, self.phone = open_hours, phone
 
     def __str__(self):
         return f"Library: {self.city}, {self.street}, {self.zip_code}, {self.open_hours}, {self.phone}"
 
+
 class Employee:
     def __init__(self, first_name, last_name, hire_date, birth_date, city, street, zip_code, phone):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.hire_date = hire_date
-        self.birth_date = birth_date
-        self.city = city
-        self.street = street
-        self.zip_code = zip_code
-        self.phone = phone
+        self.first_name, self.last_name = first_name, last_name
+        self.hire_date, self.birth_date = datetime.strptime(hire_date, "%Y-%m-%d"), datetime.strptime(birth_date, "%Y-%m-%d")
+        self.city, self.street, self.zip_code, self.phone = city, street, zip_code, phone
 
     def __str__(self):
-        return f"Employee: {self.first_name} {self.last_name}, {self.hire_date}, {self.birth_date}, {self.city}, {self.street}, {self.zip_code}, {self.phone}"
+        return (
+            f"Employee: {self.first_name} {self.last_name}, "
+            f"Hire Date: {self.hire_date.strftime('%Y-%m-%d')}, "
+            f"Birth Date: {self.birth_date.strftime('%Y-%m-%d')}, "
+            f"{self.city}, {self.street}, {self.zip_code}, {self.phone}"
+        )
+
 
 class Book:
     def __init__(self, library, publication_date, author_name, author_surname, number_of_pages):
-        self.library = library
-        self.publication_date = publication_date
-        self.author_name = author_name
-        self.author_surname = author_surname
-        self.number_of_pages = number_of_pages
+        self.library, self.publication_date = library, datetime.strptime(publication_date, "%Y-%m-%d")
+        self.author_name, self.author_surname, self.number_of_pages = author_name, author_surname, number_of_pages
 
     def __str__(self):
-        return f"Book: {self.author_name} {self.author_surname}, {self.publication_date}, {self.number_of_pages} pages\n{self.library}"
+        return (
+            f"Book: {self.author_name} {self.author_surname}, "
+            f"Publication Date: {self.publication_date.strftime('%Y-%m-%d')}, "
+            f"{self.number_of_pages} pages\n{self.library}"
+        )
+
 
 class Order:
-    def __init__(self, employee, student, books, order_date):
-        self.employee = employee
-        self.student = student
-        self.books = books
-        self.order_date = order_date
+    def __init__(self, employee, student_name, books, order_date):
+        self.employee, self.student_name, self.books = employee, student_name, books
+        self.order_date = datetime.strptime(order_date, "%Y-%m-%d")
 
     def __str__(self):
         book_list = "\n".join([str(book) for book in self.books])
-        return f"Order: {self.order_date}\n{self.employee}\nStudent: {self.student}\nBooks:\n{book_list}"
+        return (
+            f"Order Date: {self.order_date.strftime('%Y-%m-%d')}\n"
+            f"{self.employee}\nStudent Name: {self.student_name}\nBooks:\n{book_list}"
+        )
+
 
 library1 = Library("City1", "Street1", "11111", "9:00 AM - 5:00 PM", "111-222-333")
 library2 = Library("City2", "Street2", "22222", "10:00 AM - 6:00 PM", "444-555-666")
